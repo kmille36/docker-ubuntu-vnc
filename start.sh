12e90e382c1edbb941ebe4ec40516e08be1b659b
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-13 15:38:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,17 +3,8 @@
 umask 0077                # use safe default permissions
 mkdir -p "$HOME/.vnc"     # create config directory
 chmod go-rwx "$HOME/.vnc" # enforce safe permissions
-
-tigervnc_path="/tigervnc-${tigervnc_version}.x86_64/usr/bin"
-
-# Start TigerVNC
-if [ ! -z $VNC_PASSWD ]; then
-	${tigervnc_path}/vncpasswd -f <<< "$VNC_PASSWD" > "$HOME/.vnc/passwd"
-	${tigervnc_path}/vncserver -rfbport 5900
-else
-	${tigervnc_path}/vncpasswd -f <<< "" > "$HOME/.vnc/passwd"
-	${tigervnc_path}/vncserver -rfbport 5900 -SecurityTypes None
-fi
+vncpasswd -f <<< "123456" > "$HOME/.vnc/passwd" # set password to 123456
+vncserver -rfbport 5901 # start vncserver with port 5901
 
 # Start noVNC
-/noVNC-${noVNC_version}/utils/launch.sh
+./noVNC/utils/novnc_proxy --listen 6080 --vnc localhost:5901
