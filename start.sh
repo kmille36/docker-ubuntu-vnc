665e0a5e7810630cbab9cc843500ab08b62fd9d5
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-13 15:55:12 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ umask 0077                # use safe default permissions
 mkdir -p "$HOME/.vnc"     # create config directory
 chmod go-rwx "$HOME/.vnc" # enforce safe permissions
 vncpasswd -f <<< "123456" > "$HOME/.vnc/passwd" # set password to 123456
-vncserver -rfbport 5901 # start vncserver with port 5901
+vncserver -rfbport 5900 # start vncserver with port 5901
 
 # Start noVNC
-./noVNC/utils/novnc_proxy --listen 6080 --vnc localhost:5901
+./noVNC/utils/novnc_proxy --listen 6080 --vnc localhost:5900
